b063b0f3a03beff99da3e513ade52c4382f59da8
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-29 21:35:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+dictionary = {
+  'hello' => 'hi',
+  'to' => '2',
+  'two' => '2',
+  'too' => '2',
+  'for' => '4',
+  'four' => '4',
+  'be' => 'b',
+  'you' => 'u',
+  'at' => '@',
+  'and' => '&'
+}
+
+def word_subtituter(tweet)
+  tweet_arr = tweet.split(' ')
+  for i in tweet_arr
+    if dictionary.includes?(tweet_arr[i])
+      tweet_arr[i] = dictionary.value
+    end
+  end
+  tweet_arr.join(' ')
+end
